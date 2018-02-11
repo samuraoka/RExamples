64571b484ac6d9ecb18471218eed8096957336e4
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-02-12 07:43:10 +0900

    Lazy Evaluation

--- RProgrammingFundamentals/FunctionOverview.R
+++ RProgrammingFundamentals/FunctionOverview.R
@@ -34,3 +34,18 @@ student.chemistry.total.marks <- GetTotalMarks(viva.marks = student.chemistry.vi
                                                creativity.marks = 2, attendence.marks = 3)
 
 student.chemistry.total.marks
+
+# Lazy Evaluation
+GetTotalMarks <- function(quiz.marks, viva.marks, extra.marks = average.viva.marks) {
+    average.viva.marks <- mean(viva.marks)
+    total.marks <- quiz.marks + viva.marks + extra.marks
+    total.marks
+}
+
+GetTotalMarks(quiz.marks <- c(70L, 75L, 80L, 85L),
+              viva.marks <- c(7L, 5L, 8L, 6L))
+
+GetTotalMarks(quiz.marks <- c(70L, 75L, 80L, 85L),
+              viva.marks <- c(7L, 5L, 8L, 6L),
+              c(1L, 1L, 1L, 1L))
+
