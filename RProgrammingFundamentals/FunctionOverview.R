0a113574aa73c1609fc427a935782fa0ec896d4a
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-02-12 08:51:54 +0900

    functions as object anonymous function

--- RProgrammingFundamentals/FunctionOverview.R
+++ RProgrammingFundamentals/FunctionOverview.R
@@ -61,3 +61,42 @@ GetMarksSummary <- function(quiz.marks, viva.marks) {
 }
 
 GetMarksSummary(quiz.marks = c(70L, 75L, 80L, 85L), viva.marks = c(7L, 5L, 8L, 6L))
+
+#
+# Functions as Object
+#
+GetTotalMarks <- function(quiz.marks, viva.marks) {
+    total.marks <- quiz.marks + viva.marks
+    total.marks
+}
+
+# 01 Look into them
+GetTotalMarks # return the function itself
+formals(GetTotalMarks) # Access arguments
+body(GetTotalMakrs) # Access body
+
+# 02 Assign them
+MyGetTotalMarks <- GetTotalMarks
+MyGetTotalMarks
+
+# 03 Use them as arguments to another function
+GetTotalMarks(quiz.marks = c(70L, 75L, 80L, 85L), viva.marks = c(7L, 5L, 8L, 6L))
+
+# pass the function as a argument in do.call function
+do.call(GetTotalMarks, list(quiz.marks = c(70L, 75L, 80L, 85L), viva.marks = c(7L, 5L, 8L, 6L)))
+
+#
+# Anonymous Function
+#
+# Named function
+GetTotalMarks <- function(quiz.marks, viva.marks) {
+    total.marks <- quiz.marks + viva.marks
+    total.marks
+}
+
+# using named function
+do.call(GetTotalMarks, list(quiz.marks = c(70L, 75L, 80L, 85L), viva.marks = c(7L, 5L, 8L, 6L)))
+
+# using anonymous function
+do.call(function(quiz.marks, viva.marks) { quiz.marks + viva.marks },
+        list(quiz.marks = c(70L, 75L, 80L, 85L), viva.marks = c(7L, 5L, 8L, 6L)))
