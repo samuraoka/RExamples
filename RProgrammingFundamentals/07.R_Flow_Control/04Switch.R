7afb60bf633b7615ae1e56dd9ff6fca5cf244073
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-22 03:19:15 +0900

    demo for Switch with curly braces.

---
+++ RProgrammingFundamentals/07.R_Flow_Control/04Switch.R
@@ -0,0 +1,15 @@
+GetMarkSummary <- function(test.marks, summary.type) {
+    result <- switch(summary.type,
+        "mean" = {
+        mean(test.marks)
+    },
+        "median" = {
+        median(test.marks)
+    },
+        "variance" = {
+        var(test.marks)
+    },
+        "Not Implemented"
+    )
+    result
+}
