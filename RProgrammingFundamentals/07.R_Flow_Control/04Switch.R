25bc79a675660905798aa940547bf87a86d33580
Satoshi Muraoka <sa.muraoka@outlook.com>
2018-03-22 03:20:12 +0900

    demo for Switch trimmed version

--- RProgrammingFundamentals/07.R_Flow_Control/04Switch.R
+++ RProgrammingFundamentals/07.R_Flow_Control/04Switch.R
@@ -1,15 +1,8 @@
 GetMarkSummary <- function(test.marks, summary.type) {
     result <- switch(summary.type,
-        "mean" = {
-        mean(test.marks)
-    },
-        "median" = {
-        median(test.marks)
-    },
-        "variance" = {
-        var(test.marks)
-    },
-        "Not Implemented"
-    )
+        "mean" = mean(test.marks),
+        "median" = median(test.marks),
+        "variance" = var(test.marks),
+        "Not Implemented")
     result
 }
